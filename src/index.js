56e1e28acc1ef518f2be30747cb571567eb65e0a
Tony Liang <tonyjjliang@gmail.com>
2021-05-18 13:19:02 -0700

    added necessary modules

--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// pass a callback function to screen so the screen can change states
+function CalculatorButton(props){
+    return (
+        <button className={props.className} onClick={()=>{props.action(props.symbol)}}>
+            {props.symbol}
+        </button>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Screen(props){
+    return (
+        <div className="screen">
+            <div className="prev-display">
+                {props.prevContent}
+            </div>
+            <div className="current-display">
+                {props.currentContent}
+            </div>
+        </div>
+    )
+}
+
+class Calculator extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            content:'',
+        };
+        this.clearDisplay = this.clearDisplay.bind(this)
+        this.addToDisplay = this.addToDisplay.bind(this)
+        this.deleteCharacter = this.deleteCharacter.bind(this)
+        this.equals = this.equals.bind(this)
+    }
+
+    clearDisplay(blank){
+        this.setState({content:''})
+    }
+
+    addToDisplay(symbol){
+        let newContent = this.state.content;
+        newContent += symbol;
+        this.setState({content: newContent})
+    }
+
+    deleteCharacter(blank){
+        let newContent = this.state.content.slice(0, -1);
+        this.setState({content: newContent})
+    }
+
+    equals(blank){
+
+    }
+
+    render(){
+        return(
+            <div className="grid">
+                <Screen prevContent="123" currentContent={this.state.content}></Screen>
+                <CalculatorButton className="all-clear" symbol="AC" action={this.clearDisplay}></CalculatorButton>
+                <CalculatorButton className="delete" symbol="DEL" action={this.deleteCharacter}></CalculatorButton>
+                <CalculatorButton className="op" symbol="/" action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="num-data" symbol="7" action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="num-data" symbol="8" action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="num-data" symbol="9" action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="op" symbol="*" action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="num-data"  symbol="4" action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="num-data"  symbol="5" action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="num-data"  symbol="6" action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="op" symbol="+" action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="num-data"  symbol="1" action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="num-data"  symbol="2" action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="num-data"  symbol="3" action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="op" symbol="-" action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="num-data"  symbol="." action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="num-data"  symbol="0" action={this.addToDisplay}></CalculatorButton>
+                <CalculatorButton className="equals" symbol="=" action={this.equals}></CalculatorButton>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Calculator></Calculator>, document.getElementById('root'))
